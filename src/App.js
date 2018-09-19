d35a07a7a0749d0a27c36b9fb7050ead414611d8
tprax <tprax01@gmail.com>
2018-09-19 08:29:04 -0600

    working counter

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 0}
+    this.inc = this.inc.bind(this)
+    this.dec = this.dec.bind(this)
+  }
+
+  inc(){
+    this.setState((state) => {
+      return {value: state.value + 1}
+    })
+ 
+  }
+
+  dec() {
+    this.setState((state) => {
+      return {value: state.value - 1}
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Counter Example</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          {this.state.value}
         </p>
+          <button onClick={this.inc}>+</button>
+          <button onClick={this.dec}>-</button>
       </div>
     );
   }
